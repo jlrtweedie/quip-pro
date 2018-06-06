48fa6cdc9af57bb3a8370b81db42b76da591b7b8
Lauren <jamie.lr.tweedie@gmail.com>
2018-06-05 23:12:31 -0700

    File cleanup

--- static/js/actions/testRequest.js
+++
@@ -1,9 +0,0 @@
-function TestRequest() {
-  const TEST_REQUEST = 'TEST_REQUEST'
-  return {
-    type: TEST_REQUEST,
-    outbound: 'test_request'
-  }
-}
-
-export default TestRequest
--- static/js/actions/testResponse.js
+++
@@ -1,9 +0,0 @@
-function TestResponse() {
-  const TEST_RESPONSE = 'TEST_RESPONSE'
-  return {
-    type: TEST_RESPONSE,
-    outbound: 'test_response'
-  }
-}
-
-export default TestResponse
